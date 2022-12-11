3768295de3ac463ae2dda301f993538c723ce7dd
AbdullahEmir <116547018+AbdullahEmir@users.noreply.github.com>
2022-12-11 20:20:24 +0300

    Add files via upload

---
+++ IOExcaption.java
@@ -0,0 +1,25 @@
+import java.io.InvalidObjectException;
+import java.util.Scanner;
+
+public class IOExcaption {
+public static void main(String args[]) throw IOExcaption{
+	Scanner giriş=new Scanner(System.in);
+int not;
+System.out.println("Sınav notu(3-4-5);");
+not=giriş.nextadaouble();
+swicth(not){
+	case3:
+		System.out.println("biraz daha çalış");
+	break:
+		case 4:
+			System.out.println("biraz daha çalış");
+		break:
+			case 5:
+				System.out.println("biraz daha çalış");
+			break:
+				default;
+}
+System.out.println("Hiç sana yakışmadı");
+break;
+}
+}

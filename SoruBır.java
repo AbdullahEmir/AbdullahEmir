2d8302137f58d57cb3705424f6331a6a61a4a8a2
AbdullahEmir <116547018+AbdullahEmir@users.noreply.github.com>
2022-12-11 19:42:05 +0300

    Add files via upload

---
+++ SoruBır.java
@@ -0,0 +1,12 @@
+
+public class SoruBır {
+	public static void vıze(System[]args) {
+		
+	int sayı1=23;
+	double sayı2=44.44444;
+	int toplam= sayı1 +(int)sayı2;
+    System.out.println(toplam);
+
+		
+	}
+}

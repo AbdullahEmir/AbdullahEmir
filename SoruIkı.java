4df3676ae4924f6cb550071539268d5752329a37
AbdullahEmir <116547018+AbdullahEmir@users.noreply.github.com>
2022-12-11 19:58:06 +0300

    Add files via upload

--- SoruIkı.java
+++ SoruIkı.java
@@ -1,19 +1,11 @@
 import java.util.Scanner;
 public class SoruIkı {
 public static void main(String[] args) {
-	Scanner giriş=new Scanner(System.in);
-	double gelir,vergi;
-	System.out.println("Gelirinizi girin");
-	gelir=giriş.nextDouble();
-	if (gelir<=150000) {
-		System.out.println("vergi "+gelir/4+" TL");
-	}else if(gelir<=300000) {
-		System.out.println("vergi "+gelir*0.3+" TL");
-	}else if(gelir<=600000) {
-		System.out.println("vergi "+gelir*0.35);
-	}else if(gelir<=1200000) {
-		System.out.println("vergi "+gelir*0.4);
-	}
-	System.out.println("vergi"+gelir/2+" TL");
+double mil=100;
+double kilometre=1.609;
+double kilometre1;
+kilometre=mil*kilometre;
+System.out.println(kilometre);
+
 }
 }

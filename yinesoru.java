02b91c83095b9c1e094bbc4b49e54074680f0c37
AbdullahEmir <116547018+AbdullahEmir@users.noreply.github.com>
2022-12-11 20:11:11 +0300

    Add files via upload

---
+++ yinesoru.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class yinesoru {
+
+	public static void main(String[] args) {
+		Scanner giriş=new Scanner(System.in);
+		System.out.println("Byte tipinde bir sayı gir");
+		byte sayı=giriş.nextByte();
+
+	}
+
+}
